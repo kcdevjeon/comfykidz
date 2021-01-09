8eb25452528d87fd01f86ab657ef78cbf0a5bd44
kcdevjeon <kkcdevjeon@gmail.com>
2021-01-09 20:20:46 +0900

    data binding

--- app/src/main/java/com/kccorp/comfykids/FirstFragment.kt
+++ app/src/main/java/com/kccorp/comfykids/FirstFragment.kt
@@ -1,24 +1,24 @@
 package com.kccorp.comfykids
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.kccorp.comfykids.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    lateinit var binding: FragmentFirstBinding
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
